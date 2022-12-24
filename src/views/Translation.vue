b6859af678017965d2394868762c1208417f3540
KUNLIN YU <3238127737@qq.com>
2022-12-24 16:08:24 +0800

    12.23 some changes

--- src/views/Translation.vue
+++ src/views/Translation.vue
@@ -24,14 +24,22 @@
         <el-scrollbar height="80%">
           <el-table class="s-fileTable"
                     ref="FileTableRef"
-                    style="width: 70%"
-                    @row-click="FileOperation"
+                    style="width: 90%"
+                    @row-click="FileOperation(row, column, event)"
                     :row-class-name="tableRowClassName"
                     :data="fileData">
             <el-table-column prop="fileName"
                              label="fileName"
                              width="auto" />
+            <el-table-column
+                width="76">
+              <template #default="{ row }">
+                <span style="cursor:pointer"><Delete  @click="fileDelete(row)"  style="width: 1em; height: 1em; margin-right: 8px" /></span>
+
+              </template>
+            </el-table-column>
           </el-table>
+          <el-button @click="leftAddClick" type="primary">+ 新增file</el-button>
         </el-scrollbar>
       </el-aside>
 
@@ -43,6 +51,7 @@
             <el-table :key="keyNum"
                       ref="MainTableRef"
                       :data="tableData"
+                      :row-key="(row) => row.id"
                       style="width: 100%"
                       @selection-change="handleSelectionChange"
                       @row-click="chooseOperation"
@@ -58,6 +67,13 @@
               <el-table-column prop="relation"
                                label="relation"
                                width="auto" />
+              <el-table-column
+                  width="76">
+                <template #default="{ row }">
+                  <span style="cursor:pointer"><Delete  @click="TextDelete(row)"  style="width: 1em; height: 1em; margin-right: 8px" /></span>
+
+                </template>
+              </el-table-column>
             </el-table>
           </el-scrollbar>
 
@@ -97,11 +113,45 @@
                        @click="myMark">Mark</el-button>
             <el-button type="primary"
                        @click="myRemove">Clear</el-button>
+            <el-button type="primary"
+                       @click="myAdd">Add</el-button>
           </div>
         </el-footer>
       </el-container>
     </el-container>
+    <el-dialog v-model="dialogFileVisible" title="add File">
+      <el-form :rules="rules"  ref="ruleFormRef" :model="fileForm"  label-width="120px">
+        <el-form-item label="File name" prop="name">
+          <el-input v-model="fileForm.name" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmitFile('ruleFormRef')">Submit</el-button>
+          <el-button @click="dialogFileVisible=false">Cancel</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+
+    <el-dialog v-model="dialogTextVisible" title="add Text">
+      <el-form :rules="Textrules"  ref="ruleFormText" :model="textForm"  label-width="120px">
+        <el-form-item label="Text origin" prop="origin">
+          <el-input v-model="textForm.origin"/>
+        </el-form-item>
+        <el-form-item label="Text translation" prop="translation">
+          <el-input  v-model="textForm.translation" />
+        </el-form-item>
+        <el-form-item label="Text relation" prop="relation">
+          <el-input v-model="textForm. relation" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmitText('ruleFormText')">Submit</el-button>
+          <el-button @click="dialogTextVisible=false">Cancel</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
+
+
 </template>
 
 <script>
@@ -116,10 +166,39 @@ import {
   mark,
   updateTranslation
 } from "@/http/api";
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 export default {
+
   data() {
     return {
+      textForm:{
+        origin:'',
+        translation:'',
+        relation:''
+      },
+      Textrules:{
+        origin: [
+          { required: true, message: 'Please input origin', trigger: 'blur' }
+        ],
+        translation: [
+          { required: true, message: 'Please input file translation', trigger: 'blur' }
+        ],
+        relation: [
+          { required: true, message: 'Please input file relation', trigger: 'blur' }
+        ],
+      },
+      rules:{
+        name: [
+          { required: true, message: 'Please input file name', trigger: 'blur' }
+        ],
+      },
+      dialogTextVisible:false,
+
+      fileForm:{
+        name:''
+      },
+      dialogFileVisible:false,
       activeindex: null,
       stylecolor: false,
       keyNum: 0,
@@ -131,7 +210,7 @@ export default {
         translation: '',
         relation: '',
         id: 0,
-        remark: false
+        remark: true
       },
       //update the structure of id(num), and remark(boolean) by Kunlin Yu 2022/12/21
       //my data by Yutan Wu, why you do not use list? arr is hard to read.
@@ -163,7 +242,7 @@ export default {
           relation: '[glass]->[玻璃]; Proofread',
           id: 1,
           file:1,
-          remark: false
+          remark: true
         },
         {
           origin: 'Computer science is beautiful.',
@@ -171,7 +250,7 @@ export default {
           relation: '[Computer Science]->[计算机科学]',
           id: 2,
           file:1,
-          remark: false
+          remark: true
         },
         {
           origin: 'Dummy',
@@ -179,7 +258,7 @@ export default {
           relation: 'Proofread',
           id: 3,
           file:1,
-          remake: false
+          remark: false
         },
         {
           origin: 'Dummy',
@@ -187,7 +266,7 @@ export default {
           relation: 'Proofread',
           id: 4,
           file:1,
-          remake: false
+          remark: false
         },
         {
           origin: 'Dummy',
@@ -195,7 +274,7 @@ export default {
           relation: 'Proofread',
           id: 5,
           file:1,
-          remake: false
+          remark: true
         },
         {
           origin: 'Dummy',
@@ -203,7 +282,7 @@ export default {
           relation: 'Proofread',
           id: 6,
           file:1,
-          remake: false
+          remark: false
         },
         {
           origin: 'Dummy',
@@ -211,7 +290,7 @@ export default {
           relation: 'Proofread',
           id: 7,
           file:1,
-          remake: false
+          remark: false
         },
         {
           origin: 'Dummy',
@@ -219,7 +298,7 @@ export default {
           relation: 'Proofread',
           id: 8,
           file:1,
-          remake: false
+          remark: true
         },
         {
           origin: 'Dummy',
@@ -227,7 +306,7 @@ export default {
           relation: 'Proofread',
           id: 9,
           file:1,
-          remake: false
+          remark: false
         },
         {
           origin: 'Dummy',
@@ -235,7 +314,7 @@ export default {
           relation: 'Proofread',
           id: 10,
           file:1,
-          remake: false
+          remark: false
         },
       ]
       ,
@@ -249,6 +328,107 @@ export default {
     this.onload();
   },
   methods: {
+    // 左侧新增file
+    leftAddClick(){
+      this.dialogFileVisible=true;
+      this.fileForm.name=''
+    },
+    // 确定新增text
+    onSubmitText(formName){
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('add sucess')
+          this.dialogTextVisible=false
+          this.tableData.push({
+            origin: this.textForm.origin,
+            translation: this.textForm.translation,
+            relation: this.textForm.relation
+          }
+
+          )
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+},
+    myAdd(){
+     this.textForm={
+       origin:'',
+       translation:'',
+       relation:''
+     }
+     this.dialogTextVisible=true
+    },
+    // 确定添加一行file
+    onSubmitFile(formName){
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('add sucess')
+         this.dialogFileVisible=false
+          this.fileData.push(
+              {
+                fileName:this.fileForm.name ,
+                fileDiscription: "This is a File discription",
+                CateId:0
+              }
+          )
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    // 删除一行text
+    TextDelete(row){
+      ElMessageBox.confirm(
+          'proxy will permanently delete the file. Continue?',
+          'Warning',
+          {
+            confirmButtonText: 'OK',
+            cancelButtonText: 'Cancel',
+            type: 'warning',
+          }
+      )
+          .then(() => {
+            this.tableData.splice(row.index,1)
+            ElMessage({
+              type: 'success',
+              message: 'Delete completed',
+            })
+          })
+          .catch(() => {
+            ElMessage({
+              type: 'info',
+              message: 'Delete canceled',
+            })
+          })
+    },
+    // 删除一行file
+    fileDelete(row){
+      ElMessageBox.confirm(
+          'proxy will permanently delete the file. Continue?',
+          'Warning',
+          {
+            confirmButtonText: 'OK',
+            cancelButtonText: 'Cancel',
+            type: 'warning',
+          }
+      )
+          .then(() => {
+            this.fileData.splice(row.index,1)
+            ElMessage({
+              type: 'success',
+              message: 'Delete completed',
+            })
+          })
+          .catch(() => {
+            ElMessage({
+              type: 'info',
+              message: 'Delete canceled',
+            })
+          })
+    },
     handleCurrentChange() { },
     Refresh() {
       //should use fileId instead of 1 in two parts
@@ -276,9 +456,9 @@ export default {
     myMark() {
       this.activeindex = this.rowindexs
       this.stylecolor = !this.stylecolor
-      if (this.row.remark===false){
+      if (this.row.remark===false){//这里的row是点击后获取到的那一行数据对吧？
         mark(this.row.id).then((res)=>{
-
+            console.log(res.data)
         })
       }else{
         cleanMark(this.row.id).then((res)=>{
@@ -292,6 +472,7 @@ export default {
     onload() {
       //this.chooseOperation(this.tableData[0]);
     },
+    // 右侧text表格点击一行
     chooseOperation(row) {
       // this.activeindex = row.index
       this.rowIndex=row.index
@@ -303,13 +484,15 @@ export default {
     },
     selectedstyle({ row, rowIndex }) {
       console.log(this.stylecolor)
-      if (this.activeindex === rowIndex) {
+      if (row.remark===true) {
         return {
           "background-color": "red",
         };
       }
     },
-    fileOperation(row){
+    // 点击左侧某一行
+    FileOperation(row){
+      alert('点击左侧某一行')
       console.log(row.id)
       this.fileNum=row.id
     },
@@ -349,5 +532,6 @@ export default {
 }
 .s-fileTable{
   margin: 20px;    /*调整边距*/
+  margin-left:10px;
 }
 </style>
